4a40cc1f6a404aedc7e61977c7b4532a412410d0
Danielle Bergbower <dkw5011@gmail.com>
2019-05-06 21:12:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,8 @@
 def prime?(i)
-  prime_numbers_test = [2, 3, 5, 7, 11, 13]
-  squared_number = i * * .5
-  prime_numbers_test.each do |num|
-   remainder = squared_number % num 
-   if remainder != 0 && squared_number > 1
-     return true
-    else
+  if i <2
      return false
     end
-  end
+    
 end
 
   
